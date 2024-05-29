fb15bb726df86b8698c0c8f95971ef79b6ca70eb
JSilvaRibeiro <123755997+JSilvaRibeiro@users.noreply.github.com>
2024-05-28 22:48:38 -0400

    Workng on temp conv

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+  let celNum = num - 32;
+  let result = Math.round(celNum * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  let fahrNum = num + 32;
+  let result = Math.round(fahrNum * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
